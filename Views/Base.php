1110e284df8860d4431bf5a9ff33cb929a8e06d2
Michel Grillon <mgrillon@gmail.com>
2024-10-04 19:08:12 +0200

    Update Base.php

--- Views/Base.php
+++ Views/Base.php
@@ -56,7 +56,7 @@
         </footer>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    <script src="https://cefii-developpements.fr/michel1385/ECF2/Application_bibliotheque/public/js/script.js"></script>
+    <script src="/../public/js/script.js"></script>
 </body>
 
 </html>
